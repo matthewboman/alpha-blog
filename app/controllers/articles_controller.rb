2be4a87d5a5485023d5bc749c96aaa6b2fb1a9a8
crashspringfield <ccrsh@riseup.net>
2019-07-25 09:45:52 -0400

    finish article CRUD

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,66 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [
+    :edit,
+    :update,
+    :show,
+    :destroy
+  ]
+
+  # Get
+  def index
+    @articles = Article.all
+  end
+
+  def edit
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+  end
+
+
+  # Post
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else # e.g. validations fail
+      render 'new'
+    end
+  end
+
+
+  # Put/Patch
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else # e.g. validations fail
+      render 'edit'
+    end
+  end
+
+
+  # Delete
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
